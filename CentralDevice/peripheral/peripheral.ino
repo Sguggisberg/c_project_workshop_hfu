49c2451dd286b7128b1077d03ebec4aed13121a8
ora8 <ora8@gmx.ch>
2026-03-17 12:10:19 +0100

    Message header added

--- CentralDevice/peripheral/peripheral.ino
+++ CentralDevice/peripheral/peripheral.ino
@@ -1,14 +1,19 @@
 #include <ArduinoBLE.h>
+#include <string.h>
+
+#include "services.h"
 
 int buttonPin = 2;
 boolean ledSwitch;
 
 BLEService LEDService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
 // BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
-BLEByteCharacteristic LEDCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify | BLEWrite);
+BLECharacteristic LEDCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify | BLEWrite,
+        sizeof(Message), true);
 void setup() {
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.println("Initialized");
+  
   // begin initialization
   if (!BLE.begin()) {
     Serial.println("starting Bluetooth® Low Energy failed!");
@@ -24,6 +29,7 @@ void setup() {
   BLE.advertise();
   Serial.println("BLE LED Peripheral, waiting for connections....");
 }
+
 void loop() {
   // listen for BLE peripherals to connect:
   BLEDevice central = BLE.central();
@@ -33,24 +39,25 @@ void loop() {
     // print the central's MAC address:
     Serial.println(central.address());
     // while the central is still connected to peripheral:
+
     while (central.connected()) {
 
-      int buttonState = digitalRead(buttonPin);
+      delay(500);
+      Message message = {
+        BOMB_ATTACK,
+        {},
+        {},
+        10,
+        7
+      };
+      memcpy(&message.senderId, central.address().c_str(), central.address().length());
+      memcpy(&message.receiverId, BLE.address().c_str(), BLE.address().length());
+    
+      Serial.print("Write value: ");
+      //Serial.println(message);
+      LEDCharacteristic.writeValue(&message, sizeof(Message)); 
+     }
 
-      if (buttonState == LOW) {
-        ledSwitch = !ledSwitch;
-        delay(500);
-
-        if (ledSwitch) {
-          Serial.println("ON");
-          LEDCharacteristic.writeValue((byte)0x01);
-        }
-        else {
-          LEDCharacteristic.writeValue((byte)0x00);
-          Serial.println("OFF");
-        }
-      }
-    }
     // when the central disconnects, print it out:
     Serial.print(F("Disconnected from central: "));
     Serial.println(central.address());
---
+++ CentralDevice/peripheral/services.h
@@ -0,0 +1,29 @@
+#ifndef _SERVICE_H_
+#define _SERVICE_H_
+
+#include <stddef.h>
+
+const size_t MAC_ADDR_LENGTH = 6;
+
+typedef uint8_t MacAddress[MAC_ADDR_LENGTH];
+
+enum MessageType {
+  START,
+  PLAY,
+  BOMB_ATTACK,
+  SPY_ATTACK,
+  HIT,
+  MISSED,
+  STOP,
+};
+
+struct Message {
+  MessageType type;
+  MacAddress senderId;
+  MacAddress receiverId;
+  uint8_t x;  // Only necessary for moves
+  uint8_t y;  // Only necessary for moves
+};
+
+
+#endif
