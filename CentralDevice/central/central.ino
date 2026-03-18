1a4d94e58bac95ff201d56bf8b4a871a7487d970
ora8 <ora8@gmx.ch>
2026-03-18 08:38:24 +0100

    GameService

--- CentralDevice/central/central.ino
+++ CentralDevice/central/central.ino
@@ -7,6 +7,7 @@
 bool logging = false;
 bool isCentral = false;
 
+
 /*
 419d3655-282c-4a0e-9b7d-fbed8e928581
  b7a73424-a7d9-4aff-958d-04f4612d461b
@@ -21,6 +22,7 @@ bool isCentral() {
 */
 
 void setup() {
+  Serial.println("Begin");
   Serial.begin(9600);
   Serial.println("Setup");
   if (isCentral) {
@@ -36,3 +38,65 @@ void loop() {
     loopPeripheral();
   }
 }
+
+/*
+#include <ArduinoBLE.h>
+
+int buttonPin = 2;
+boolean ledSwitch;
+
+BLEService LEDService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
+// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic LEDCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify | BLEWrite);
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy failed!");
+  }
+  // set advertised local name and service UUID:
+  BLE.setLocalName("Button Device");
+  BLE.setAdvertisedService(LEDService);
+  // add the characteristic to the service
+  LEDService.addCharacteristic(LEDCharacteristic);
+  // add service
+  BLE.addService(LEDService);
+  // start advertising
+  BLE.advertise();
+  Serial.println("BLE LED Peripheral, waiting for connections....");
+}
+
+void loop() {
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+    // while the central is still connected to peripheral:
+    while (central.connected()) {
+
+      int buttonState = digitalRead(buttonPin);
+
+      if (buttonState == LOW) {
+        ledSwitch = !ledSwitch;
+        delay(500);
+
+        if (ledSwitch) {
+          Serial.println("ON");
+          LEDCharacteristic.writeValue((byte)0x01);
+        }
+        else {
+          LEDCharacteristic.writeValue((byte)0x00);
+          Serial.println("OFF");
+        }
+      }
+    }
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  }
+}
+*/
--- CentralDevice/central/peripheral.cpp
+++ CentralDevice/central/peripheral.cpp
@@ -1,7 +1,15 @@
 #include "peripheral.h"
 
 BLECharacteristic messageCharacteristic;
+int buttonPin = 2;
+boolean ledSwitch;
 
+BLEService GameService("19B10000-E8F2-537E-4F6C-D104768A1214");  // BLE LED Service
+// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLECharacteristic MessageCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify | BLEWrite,
+     sizeof(Message), true);
+
+/*
 void setupPeripheral() {
   //if (!isCentral())
   Serial.println("setupPeripheral");
@@ -23,9 +31,30 @@ void setupPeripheral() {
   // start advertising
   BLE.advertise();
 }
+*/
 
+void setupPeripheral() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy failed!");
+  }
+  // set advertised local name and service UUID:
+  BLE.setLocalName("Game Device");
+  BLE.setAdvertisedService(GameService);
+  // add the characteristic to the service
+  GameService.addCharacteristic(MessageCharacteristic);
+  // add service
+  BLE.addService(GameService);
+  // start advertising
+  BLE.advertise();
+  Serial.println("BLE Message Peripheral, waiting for connections....");
+}
+
+int count = 1;
+
+/*
 void loopPeripheral() {
-
   Serial.println("loopPeripheral");
 
   Message message = {
@@ -53,14 +82,49 @@ void loopPeripheral() {
   Serial.print("Connected: ");
   Serial.println(central.connected());
 
+  Serial.println(message.senderId);
+  Serial.println(messageCharacteristic.uuid());
+   
   while (central.connected()) {
+    Serial.print("Write: ");
+    Serial.println(count);
     Serial.println("WriteValue: ");
     Serial.print("Write: ");
-    Serial.println(message.senderId);
-    Serial.println(messageCharacteristic.uuid());
-    messageCharacteristic.writeValue(&message, sizeof(Message), false);
+    BLEByteCharacteristic testCharacteristic("b9e3b5f7-b6b8-42a8-ae4b-9ae9f7f6b618", BLEWrite);
+    testCharacteristic.writeValue((byte) 'A'); 
+    // messageCharacteristic.writeValue(&message, sizeof(Message), false);
     Serial.print("Written: ");
     Serial.println(message.senderId);
+    count++;
     delay(500);
+    Serial.print("Connected: ");
+    Serial.println(central.connected());
+  }
+
+  */
+
+void loopPeripheral() {
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+
+    Message message;
+    message.type = BOMB_ATTACK;
+    message.x = 7;
+    message.y = 8;
+
+    // while the central is still connected to peripheral:
+    while (central.connected()) {
+
+      Serial.println("Write message");
+      MessageCharacteristic.writeValue(&message, sizeof(Message), false);
+    }
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
   }
 }
