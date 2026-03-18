f03334a7f6717f6162e7762a95a13544ddbe732a
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2026-03-18 10:31:20 +0100

    add header

--- CentralDevice/central/message.h
+++ CentralDevice/central/message.h
@@ -1,11 +1,15 @@
-#ifndef SERVICE_H
-#define SERVICE_H
+#ifndef MESSAGE_H
+#define MESSAGE_H
 
 #include <Arduino.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stddef.h>
+
+const char GAME_SERVICE[]="19b10000-e8f2-537e-4f6c-d104768a1214";
+const char GAME_REQUEST_CHARACTERSITIC_UUID[] = "19b10001-e8f2-537e-4f6c-d104768a1214";
+const char GAME_RESPONSE_CHARACTERSITIC_UUID[] = "19b10002-e8f2-537e-4f6c-d104768a1214";
 
 const size_t MAC_ADDR_LENGTH = 18;
 typedef char MacAddress[MAC_ADDR_LENGTH];
